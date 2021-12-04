b3b39a139509da39e302968c317bb088c3251c3b
Isaac L. Bleaman <bleaman@berkeley.edu>
2021-12-04 09:41:45 -0800

    Fix beys, some punctuation

--- yiddish_unicode.py
+++ yiddish_unicode.py
@@ -20,15 +20,20 @@ pairs = [
 def replace_with_precombined(string):
     for pair in pairs:
         string = re.sub(pair[0], pair[1], string)
+    string = re.sub('בּ', 'ב', string) # diacritic not used in YIVO
+    string = re.sub('בּ', 'ב', string)
     return string
 
 def replace_with_decomposed(string):
     for pair in pairs:
         string = re.sub(pair[1], pair[0], string)
     string = re.sub('ייַ', 'ײַ', string)
+    string = re.sub('בּ', 'ב', string) # diacritic not used in YIVO
+    string = re.sub('בּ', 'ב', string)
     return string
 
 def replace_punctuation(string):
-    string = re.sub('׳', "'", string)
-    string = re.sub('״', '"', string)
+    string = re.sub(r"-", r"־", string) # YIVO-style hyphen
+    string = re.sub(r'[′׳]', "'", string)
+    string = re.sub(r'[″״]', '"', string)
     return string
